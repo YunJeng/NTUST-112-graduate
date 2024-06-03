3b0f942ce8780142de2ef31f4f53f1364f7c2ac4
YunJeng <167182350+YunJeng@users.noreply.github.com>
2024-06-04 01:50:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function () {
+        // 可以在這裡添加一些前端驗證或動態效果
+    });
+});
